a83c02faeb82ecd9b3da70b631144d2c871735d1
liadraz <raz.liad@gmail.com>
2025-03-18 00:41:05 +0200

    Update namespace in run file

--- RateLimiter.Service/Run.cs
+++ RateLimiter.Service/Run.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using RateLimiter.Core.Models;
+﻿using RateLimiter.Core.Models;
 using RateLimiter.Core.Strategy;
+using RateLimiter.Core.Service;
+
 class Run
 {
     public static async Task Main()
