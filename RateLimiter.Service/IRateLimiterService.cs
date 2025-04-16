ebd24e6584f9a3b754027a81cceb7ae9a3d4f6d0
liadraz <raz.liad@gmail.com>
2025-04-16 16:43:31 +0300

    Update description

--- RateLimiter.Service/IRateLimiterService.cs
+++ RateLimiter.Service/IRateLimiterService.cs
@@ -2,12 +2,15 @@ using RateLimiter.Service.Models;
 
 namespace RateLimiter.Service
 {
-    //  RateLimiter Service Interface
-    //  
-    //  The rate limiter service is the main moudle of this service. It is responsible to hold and pass requests each refers to unique caller
-    //  Policies are being passed by the a Request class
-    // 
-    //      The Perform method is called by a caller to access an endpoint, 
+    //  RateLimiterService Interface
+    //
+    //  The IRateLimiterService interface is responsible for managing concurrent requests from several callers.
+    //  It evaluates and process requests based on defined rate-limiting policies.
+    //
+    //  Usage
+    //      Instantiate the rate limiter service (no arguments required).
+    //      Use the Perform method to process a request, passing in a Request object.
+    //      The Request class contains the specific policies that govern rate-limiting for the caller.
     //
     public interface IRateLimiterService<TArg>
     {
